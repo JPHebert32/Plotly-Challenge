75af8c0b1bcac58ea800220a7361aa00e576b847
JP Hebert <JPHebert32@gmail.com>
2020-07-06 22:14:40 -0600

    1st

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,7 @@
+// Create function for plotting data (Bar, gauge, bubble)
+function buildPlots(id) {
+    // getting data from the json file
+    d3.json("Data/samples.json").then((data)=> {
+        console.log(data);
+    });
+  }
